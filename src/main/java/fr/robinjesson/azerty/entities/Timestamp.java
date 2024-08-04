92c7d08273eb5725f0a295c600130b8dcef22cfe
robinjesson <robin.jesson@hotmail.fr>
2024-08-04 22:10:15 +0200

    fix: missing timestamp embeddable

---
+++ src/main/java/fr/robinjesson/azerty/entities/Timestamp.java
@@ -0,0 +1,21 @@
+package fr.robinjesson.azerty.entities;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+@Getter
+@Setter
+public class Timestamp {
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}

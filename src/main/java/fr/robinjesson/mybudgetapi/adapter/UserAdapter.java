ee56a696b488d3e8d685def7fe098a324930fb7e
robinjesson <robin.jesson@hotmail.fr>
2025-09-09 22:08:56 +0200

    fix: cookie working

--- src/main/java/fr/robinjesson/mybudgetapi/adapter/UserAdapter.java
+++ src/main/java/fr/robinjesson/mybudgetapi/adapter/UserAdapter.java
@@ -32,9 +32,8 @@ public class UserAdapter {
         final String token = jwtBusiness.generateToken(user);
         return ResponseCookie.from(Consts.COOKIE_NAME, token)
                 .httpOnly(true)
-                .secure(false) //TODO
-                .sameSite("None")
-                .maxAge(jwtBusiness.getJwtExpiration() / 1000)
+                .path("/")
+                .maxAge(jwtBusiness.getJwtExpiration())
                 .build();
     }
 }

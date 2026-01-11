2c836f5aa251516e36bae291651b2e4048e6bd23
robinjesson <robin.jesson@hotmail.fr>
2026-01-11 22:16:28 +0100

    feat: mark FineRepository as a non-repository bean

--- src/main/java/fr/robinjesson/mybudgetapi/repository/FineRepository.java
+++ src/main/java/fr/robinjesson/mybudgetapi/repository/FineRepository.java
@@ -2,7 +2,9 @@ package fr.robinjesson.mybudgetapi.repository;
 
 import fr.robinjesson.mybudgetapi.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface FineRepository<T, ID> extends JpaRepository<T, ID> {
 
     default T findConcreteById(final ID id) {
